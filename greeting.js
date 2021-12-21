784f3d0068e483e2b828a7597a2e66c9f88b8299
sachin patil <75795803+sachin112211@users.noreply.github.com>
2021-12-21 17:00:23 +0530

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,24 @@
+
+var msg = prompt("enter the ocassion")
+     
+    switch (msg) {
+        case "New Year":
+            console.log("Happy new year")
+            break;
+        case "Chrismas":
+            console.log("happy chrismas")
+            break;
+        case "birthday":
+            console.log("happy birthday")
+            break;
+            case "anniversary":
+            console.log("happy anniversary")
+            break;
+    
+    
+        }
+   
+
+
+
+
