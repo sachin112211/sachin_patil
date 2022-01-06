5695bdd07036bde1d10876ffbe3afcf2a25a9455
sachin patil <75795803+sachin112211@users.noreply.github.com>
2022-01-06 11:29:46 +0530

    Add files via upload

---
+++ angular-asgi-user/asg-user02/sachin/src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './IUser';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+   user : IUser = {id:0,userName:""}
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
